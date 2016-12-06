55fcacb4d68a505a7d5e8810364c106b1aa09d90
Vladymyr <kryvtsov.v@gmail.com>
2016-12-06 15:51:39 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,29 @@ $(function(){
     });
 
 
+    /* Открытие меню */
+    var main = function() {
 
+        $('.icon-menu').click(function() {
+            $('.menu').animate({
+                right: '0px'
+            }, 1000, function() {$('.blackout').css('display', 'block')});
+            
+        });
+        /* Закрытие меню */
+        $('.icon-close').click(function() {
+            $('.menu').animate({
+                right: '-470px'
+            }, 1000, function() {$('.blackout').css('display', 'none')});
+            
+        });
+        $('.blackout').click(function() {
+            $('.menu').animate({
+                right: '-470px'
+            }, 1000, function() {$('.blackout').css('display', 'none')});
+            
+        });
+    };
+
+    $(document).ready(main);
 });
